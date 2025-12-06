19b244180d7180017d5c06e0580ae46919912bda
BuickSalesman <adam.odonnell.e@gmail.com>
2025-12-05 23:33:34 -0500

    made a change

--- matter.js
+++ matter.js
@@ -3,54 +3,46 @@
 
 const { ipcRenderer } = require("electron");
 const { Engine, Render, Runner, Composite, Bodies, Events, Query } = require("matter-js");
-const { createBoundariesAndBall } = require("./matter-helpers/boundariesAndBall.js")
-const { createMouseAndConstraint } = require("./matter-helpers/mouseAndConstraint.js")
-const { ballDetector } = require("./matter-helpers/ballDetector.js")
-const { windowUpdate } = require("./matter-helpers/windowUpdate.js")
+const { createBoundariesAndBall } = require("./matter-helpers/boundariesAndBall.js");
+const { createMouseAndConstraint } = require("./matter-helpers/mouseAndConstraint.js");
+const { ballDetector } = require("./matter-helpers/ballDetector.js");
+const { windowUpdate } = require("./matter-helpers/windowUpdate.js");
 
 const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const engine = Engine.create();
-const world = engine.world
+const world = engine.world;
 
 const render = Render.create({
-    canvas,
-    engine,
-    options: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        wireframes: false,
-        background: "transparent",
-    },
+  canvas,
+  engine,
+  options: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    wireframes: false,
+    background: "transparent",
+  },
 });
 Render.run(render);
 
 const runner = Runner.create();
 Runner.run(runner, engine);
 
-const { mouse, mouseConstraint } = createMouseAndConstraint(render, engine)
+//yo whaddup
+
+const { mouse, mouseConstraint } = createMouseAndConstraint(render, engine);
 Composite.add(world, mouseConstraint);
 
 const boundariesAndBall = createBoundariesAndBall(canvas.width, canvas.height);
 Composite.add(world, boundariesAndBall);
 
 // send signal to main process to stop passthrough when hovering over ball
-ballDetector(engine, mouse, mouseConstraint, ipcRenderer)
+ballDetector(engine, mouse, mouseConstraint, ipcRenderer);
 
-windowUpdate(world, ipcRenderer) //events.on(afterupdate)
+windowUpdate(world, ipcRenderer); //events.on(afterupdate)
 
-
-//listen for added window ✅
-//listen for removed window
-//listen for updated bounds
-//listen for updates size
 //listen for not enough ball space
-//listen for update fullscreen but that might be the same as ball space
 
-
-
-// Events.on(engine, "afterUpdate", updateWindows);
-
-module.exports = { canvas, render, engine }
+module.exports = { canvas, render, engine };
